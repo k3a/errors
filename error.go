dc110d351a39c4be742b77004778af3679d1db55
Mario Hros <git@reversity.org>
2022-02-25 16:39:26 +0100

    tracing support

--- error.go
+++ error.go
@@ -1,25 +1,53 @@
 // Package errors is a drop-in replacement to standard errors, extending it with additional functionality
 package errors
 
-import "fmt"
-
-// baseError is a trivial implementation of error.
-type baseError struct {
-	text string
-}
-
-func (e *baseError) Error() string {
-	return e.text
-}
+import (
+	"errors"
+	"fmt"
+	"runtime"
+)
 
 // New returns an error that formats as the given text.
 // Each call to New returns a distinct error value even if the text is identical.
 func New(text string) error {
-	return &baseError{text}
+	return errors.New(text)
+}
+
+type internalErr struct {
+	error
+}
+
+func (h internalErr) SkipInternalErr() error { return h.error }
+
+type internalErrProvider interface {
+	SkipInternalErr() error
+}
+
+func callerPC(skip int) programCounter {
+	pc := make([]uintptr, 1)
+	if runtime.Callers(skip, pc) > 0 {
+		return programCounter(pc[0])
+	}
+	return 0
+}
+
+type programCounter uintptr
+
+func (pc programCounter) FuncName() string {
+	f := runtime.FuncForPC(uintptr(pc))
+	return f.Name()
+}
+
+func (pc programCounter) FileLine() (file string, line int) {
+	f := runtime.FuncForPC(uintptr(pc))
+	return f.FileLine(uintptr(pc))
 }
 
 type wrapError struct {
+	// the actual internal error
 	internalErr
+	programCounter
+	// message of this wrapped error
 	msg string
 }
 
@@ -37,11 +65,10 @@ func Annotate(err error, msg string) error {
 		return nil
 	}
 
-	//TODO: tracing not implemented yet
-
 	return &wrapError{
-		internalErr: internalErr{err},
-		msg:         msg + ": " + err.Error(),
+		internalErr:    internalErr{err},
+		msg:            msg + ": " + err.Error(),
+		programCounter: callerPC(3),
 	}
 }
 
@@ -51,18 +78,19 @@ func Annotatef(err error, format string, args ...interface{}) error {
 		return nil
 	}
 
-	//TODO: tracing not implemented yet
-
 	return &wrapError{
-		internalErr: internalErr{err},
-		msg:         fmt.Sprintf(format, args...) + ": " + err.Error(),
+		internalErr:    internalErr{err},
+		msg:            fmt.Sprintf(format, args...) + ": " + err.Error(),
+		programCounter: callerPC(3),
 	}
 }
 
 // Trace adds the location of the Trace call to the stack (inspired by juju/errors)
 func Trace(err error) error {
-	//TODO: not implemented yet
-	return err
+	return &wrapError{
+		internalErr:    internalErr{err},
+		programCounter: callerPC(3),
+	}
 }
 
 // Errorf is an alias of fmt.Errorf
